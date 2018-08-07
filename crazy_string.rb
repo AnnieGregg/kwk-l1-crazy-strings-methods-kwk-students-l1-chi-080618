efcbe7d90b3bd695d8230d3823654c08dd4b1c20
AnnieGregg <annie.r.gregg@gmail.com>
2018-08-07 20:37:47 +0000

    automatically backed up by learn

--- crazy_string.rb
+++ crazy_string.rb
@@ -1,7 +1,7 @@
 def crazy_strings(a,b)
-puts 'apple'.reverse.upcase
 puts  a=a.reverse.upcase
 puts  b=b.gsub("S","Z").gsub("s","z").upcase
+puts "apple".reverse.upcase
 end
 
 crazy_strings("Hello", "Friends")
